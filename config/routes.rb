6ba30240a429fad119a37d5af3693bd2ce603503
mozuku28 <mozuku28@gmail.com>
2018-10-24 11:26:41 +0000

    last

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   get 'relationships/create'
   get 'relationships/destroy'
-  get 'admins/index'
-  get 'parts/index'
-  get 'menus/index'
-  get 'loads/index'
-  get 'trainings/index'
   root  'users#index'
 
   devise_for :admins, controllers: {
@@ -35,31 +30,14 @@ Rails.application.routes.draw do
       get :following, :followers
     end
 
-    resources :follows
-    resources :followers
     resources :posts do
       resources :trainings do
         resources :loads
       end
     end
   end
-  
+
   resources :relationships, only: [:create, :destroy]
-
-
-
-  resources :trainings do
-    resources :loads
-  end
-
-  resources :menus do
-    resources :loads
-  end
-
-  resources :types
-  resources :parts
-  resources :loads
-
 
   post 'users/:user_id/posts' => 'posts#create', as: "aaa"
   delete '/users/:user_id/posts/:id' => 'posts#destroy', as: "bbb"
